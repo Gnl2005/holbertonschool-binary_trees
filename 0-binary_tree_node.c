d2feee9dab8d3754e7b2e06c5f38f754b4aaf7ae
Gnl2005 <allahverdiyevasofiya@gmail.com>
2025-10-03 21:58:06 +0000

    Binary tree

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -24,21 +24,3 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	return (yeni);
 }
-
-/**
- * main - Enter point
- *
- * Return: Always 0
- */
-int main(void)
-{
-	binary_tree_t *qutu = binary_tree_node(NULL, 1);
-
-	qutu->left = binary_tree_node(qutu, 2);
-	qutu->right = binary_tree_node(qutu, 3);
-
-	printf("Qutu: %d\n", qutu->n);
-	printf("Left: %d\n", qutu->left->n);
-	printf("Right: %d\n", qutu->right->n);
-	return (0);
-}
