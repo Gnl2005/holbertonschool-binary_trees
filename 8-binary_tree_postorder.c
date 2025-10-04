e736ba01475a3e62ce404fba1238fdd5baea0525
Gnl2005 <allahverdiyevasofiya@gmail.com>
2025-10-04 22:50:20 +0000

    func

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -12,7 +12,10 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL || func == NULL)
 		return;
 
+	if (tree->left != NULL)
 	binary_tree_inorder(tree->left, func);
+
+	if (tree->right != NULL)
 	binary_tree_inorder(tree->right, func);
 	func(tree->n);
 }
